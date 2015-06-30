da30dba459251082d1ecea1028bd585b83efd5d5
bronzeee <opticaline.z@gmail.com>
2015-06-30 16:04:20 +0800

    在初始化Requests时将查询配置文件已确定是否需要设置代理。

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,6 +4,8 @@ from .config import Configuration
 
 config = Configuration(__path__[0] + '\\..\\config')
 
-Requests.set_proxy(http=config.get_property('http-proxy'))
+http = config.get_property('http-proxy')
+if http is not None:
+    Requests.set_proxy(http=http)
 
 __all__ = ['config']
