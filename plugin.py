06db56fe0be9cbafde3c75af999a54f503255def
Zhang Xu <opticaline.z@gmail.com>
2015-07-02 20:30:19 +0800

    适配mac

--- plugin.py
+++ plugin.py
@@ -1,17 +1,8 @@
 # -*- coding: utf-8 -*-
-import json
-
-settings = json.load(open("preferences.json"))
-shortcuts_dict = {}
-for shortcut in settings.get('shortcuts', []):
-    shortcuts_dict[shortcut['shortcut'].lower()] = shortcut['message']
 
 
 def results(fields, original_query):
     message = fields['~message']
-    if message.lower() in shortcuts_dict:
-        message = shortcuts_dict[message.lower()]
-    message = settings.get("greeting", " ") + " " + message
     return {
         "title": "Say '{0}'".format(message),
         "run_args": [message],
@@ -25,8 +16,4 @@ def results(fields, original_query):
 
 
 def run(message):
-    import os, pipes
-
-    voice = settings.get('voice')
-    rate = "-r 400" if settings.get("fast", False) else ""
-    os.system('say -v "{0}" {1} "{2}"'.format(voice, rate, pipes.quote(message.encode('utf8'))))
+    pass
--- py2.7.py
+++ py2.7.py
@@ -1,12 +1,18 @@
 # -*- coding: utf-8 -*-
-import logging.config
 
-if __name__ == '__main__':
+def run():
+    import logging.config
     # 使用配置文件配置logging
     logging.config.fileConfig("config/logger.conf")
-    logger = logging.getLogger('utils')
+
+    # from utils import common
+    #
+    # print(common.format('12{he[keyword]llo}34\{pass\}56{keyword}7890', {'keyword': ' wow '}))
 
     from utils import main
 
-    infos = main.video_list(['qyqx'])
-    main.play_video(infos[0])
+    print(main.video_list(['qyqx']))
+
+
+if __name__ == '__main__':
+    run()
